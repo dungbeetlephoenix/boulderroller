0ab5f8175b803387565732499a1661ec57691bb2
betternode <betternode@proton.me>
2025-02-06 14:01:28 -0500

    Initial Commit

---
+++ BoulderRollerMVP/BoulderRollerMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoulderRollerMVPApp.swift
+//  BoulderRollerMVP
+//
+//  Created by macbook on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct BoulderRollerMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BoulderRollerMVPTests/BoulderRollerMVPTests.swift
@@ -0,0 +1,16 @@
+//
+//  BoulderRollerMVPTests.swift
+//  BoulderRollerMVPTests
+//
+//  Created by macbook on 2/6/25.
+//
+
+import Testing
+
+struct BoulderRollerMVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
